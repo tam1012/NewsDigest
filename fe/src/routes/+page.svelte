5ee42a17e3822a1e7a241453d55f5574a9375085
trongnguyen24 <trongnguyen24@gmail.com>
2026-04-07 16:45:15 +0700

    style: increase font sizes for article metadata, titles, and descriptions in the news feed

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -608,7 +608,7 @@
               onclick={() => selectArticle(article)}
             >
               <div
-                class="flex items-center text-[0.625em] text-text-secondary uppercase tracking-wider mb-2"
+                class="flex items-center text-[0.65em] text-text-secondary uppercase tracking-wider mb-2"
               >
                 <span class="truncate pr-4"
                   >{getSourceName(article.source_id)}</span
@@ -623,11 +623,13 @@
                 </span>
               </div>
               <h3
-                class="font-serif text-[1em] leading-[1.4] mb-2 font-semibold text-text-main group-hover:underline underline-offset-4 transition-all"
+                class="font-serif text-[1.125em] leading-[1.4] mb-2 font-semibold text-text-main group-hover:underline underline-offset-4 transition-all"
               >
                 {@html article.title}
               </h3>
-              <p class="text-[0.875em] text-text-secondary leading-relaxed line-clamp-10 wrap-break-word">
+              <p
+                class="text-[1em] text-text-secondary leading-relaxed line-clamp-10 wrap-break-word"
+              >
                 {article.description_vn ||
                   article.description ||
                   article.summary
@@ -919,7 +921,9 @@
                 >
                   {@html article.title}
                 </h3>
-                <p class="text-[0.875em] text-text-secondary leading-relaxed line-clamp-10 wrap-break-word">
+                <p
+                  class="text-[0.875em] text-text-secondary leading-relaxed line-clamp-10 wrap-break-word"
+                >
                   {article.description_vn ||
                     article.description ||
                     article.summary
